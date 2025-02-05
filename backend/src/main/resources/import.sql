b7f82593d94bf793250c087a6dfaa5eddc2eaa1c
victorxavier <victorxavier.oliver@gmail.com>
2025-02-05 16:25:46 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_category (name) VALUES ('books');
+INSERT INTO tb_category (name) VALUES ('electronics');
+INSERT INTO tb_category (name) VALUES ('moveis');
+INSERT INTO tb_category (name) VALUES ('tel');
+INSERT INTO tb_category (name) VALUES ('computers');
