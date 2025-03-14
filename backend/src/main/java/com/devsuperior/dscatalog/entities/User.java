2d6cb14052dcb054412e9ea988f67375a1caf1ab
victorxavier <victorxavier.oliver@gmail.com>
2025-03-14 17:06:06 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -13,8 +13,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String firstName;
     private String lastName;
+
     @Column(unique = true)
     private String email;
     private String password;
